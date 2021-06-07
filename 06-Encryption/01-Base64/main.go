193cfb8773df66cf677fe807fe38287735ecb05f
petros <pit.trak@gmail.com>
2021-06-07 19:17:33 +0300

    Base64

---
+++ 06-Encryption/01-Base64/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+)
+
+// Base64 is not encryption
+//
+// Base64 is usefull to the web because all base64 characters are printable.
+func main() {
+	msg := "This is totally fun get hands-on and learning it from the ground up. Thank you for sharing this info with me and helping me learn!"
+	encoded := encode(msg)
+	fmt.Println("ENCODED MSG", encoded)
+
+	s, err := decode(encoded)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("DECODED MSG", s)
+}
+
+func encode(msg string) string {
+	return base64.URLEncoding.EncodeToString([]byte(msg))
+}
+
+func decode(encoded string) (string, error) {
+	s, err := base64.URLEncoding.DecodeString(encoded)
+	if err != nil {
+		return "", fmt.Errorf("couldn't decode string %w", err)
+	}
+	return string(s), nil
+}
