aefb89044fc69afe01f0175ffacc71ddaea8c6cd
petros <pit.trak@gmail.com>
2021-06-10 14:34:41 +0300

    JWT in a cookie

---
+++ 08-Authentication-with-JWT/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/submit", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getJWT(msg string) (string, error) {
+	myKey := "ilovethursdayswhenitrains2much"
+
+	type myClaims struct {
+		jwt.StandardClaims
+		Email string
+	}
+
+	claims := myClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+		},
+		Email: msg,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+	ss, err := token.SignedString([]byte(myKey))
+	if err != nil {
+		return "", fmt.Errorf("couldn't signedString in getJWT in NewWithClaims %w", err)
+	}
+
+	return ss, nil
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	ss, err := getJWT(email)
+	if err != nil {
+		http.Error(w, "couldn't getJWT", http.StatusInternalServerError)
+		return
+	}
+
+	c := http.Cookie{
+		Name:  "session",
+		Value: ss,
+	}
+
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{}
+	}
+
+	isEqual := true
+
+	message := "Not logged in"
+	if isEqual {
+		message = "Logged in"
+	}
+
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>HMAC Example</title>
+	</head>
+	<body>
+		<p>Cookie Value: ` + c.Value + `</p>
+		<p>Status: ` + message + `</p>
+		<form action="/submit" method="post">
+			<input type="email" name="email"/>
+			<input type="submit"/>
+		</form>
+	</body>
+	</html>`
+
+	io.WriteString(w, html)
+}
