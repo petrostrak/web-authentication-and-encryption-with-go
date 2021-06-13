ed401e7b0fb87e5518b5e04035d8579616a69ef1
petros <pit.trak@gmail.com>
2021-06-13 16:55:02 +0300

    Getting the http client

--- 10-OAuth2/main.go
+++ 10-OAuth2/main.go
@@ -21,6 +21,7 @@ var (
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/oauth/github", startGithubOauth)
+	http.HandleFunc("/oauth/receive", completeGithubOauth)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -50,4 +51,26 @@ func index(w http.ResponseWriter, r *http.Request) {
 func startGithubOauth(w http.ResponseWriter, r *http.Request) {
 	redirectURL := githubOauthConfig.AuthCodeURL("0000")
 	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func completeGithubOauth(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	state := r.FormValue("state")
+
+	if state != "0000" {
+		http.Error(w, "state is incorrect", http.StatusBadRequest)
+		return
+	}
+
+	// Exchange returns a token from the code query received from github.
+	token, err := githubOauthConfig.Exchange(r.Context(), code)
+	if err != nil {
+		http.Error(w, "couldn't login", http.StatusInternalServerError)
+		return
+	}
+
+	// TokenSource receives the created token and creates the token source.
+	tokenSource := githubOauthConfig.TokenSource(r.Context(), token)
+	client := oauth2.NewClient(r.Context(), tokenSource)
+
 }
